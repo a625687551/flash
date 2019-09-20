9e3e38144f24bb6ddd2d320d2286188c611086ca
laitaihua <2362198731@qq.com>
2019-09-20 11:50:55 +0800

    第4次修改联系3

--- src/student/lianxi3.py
+++ src/student/lianxi3.py
@@ -4,14 +4,3 @@
 
 list = [1,2,3,3,4,5,5,6]
 print(len(list))
-print(len(set(list)))
-print(set(list))
-print(len(list))
-print(len(set(list)))
-print(set(list))
-print(len(list))
-print(len(set(list)))
-print(set(list))
-print(len(list))
-print(len(set(list)))
-print(set(list))
