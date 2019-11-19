156a1c0fbf90d9cab424f98bbdc868d03ce25a41
laitaihua <2362198731@qq.com>
2019-11-19 23:49:56 +0800

    1115-1958

--- src/banche/test_by_testjingli.py
+++ src/banche/test_by_testjingli.py
@@ -12,23 +12,22 @@ requests.packages.urllib3.disable_warnings()  # 禁用安全请求警告
 
 
 class Test_by_jingli_case1():
-    def __init__(self):
-        self.host1 = "https://api-training.flashexpress.com/"
-        self.host2 = "http://backyard-api-tra.flashexpress.com/"
-        self.headers = {
+    host1 = "https://api-training.flashexpress.com/"
+    host2 = "http://backyard-api-tra.flashexpress.com/"
+    headers = {
             "Accept-Language": "zh",
             "TIMEZONE": "+07:00"
-        }
-        self.payload = None
-        self.response = None
+    }
+    payload = None
+    response = None
 
     def get_waitAuditNum(self):
         url = self.host2 + "api/_/audit/waitAuditNum"
         self.headers.update({"X-BY-SESSION-ID": getsessionid(32419)})
-        print(25)
-        print(self.headers)
-        # response1 = requests.request("post", url=url, data=self.payload, headers=headers, verify=False)
+        response1 = requests.request("post", url=url, data=self.payload, headers=self.headers, verify=False)
+        num = int(json.loads(response1.text)["data"]["num"])
+        assert num > 1
+        return num
 
-
-lisi = Test_by_jingli_case1()
-lisi.get_waitAuditNum()
+aa = Test_by_jingli_case1()
+aa.get_waitAuditNum()
